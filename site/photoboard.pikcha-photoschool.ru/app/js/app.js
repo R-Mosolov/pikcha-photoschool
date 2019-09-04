05ad3518e10934559a102b9f418ddf52b2503bf0
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-04 12:40:54 +0300

    Created a New Directory of Photoboard

---
+++ site/photoboard.pikcha-photoschool.ru/app/js/app.js
@@ -0,0 +1,8 @@
+var titleAd = ['Владислав', 'Борис', 'Алексей'];
+var sortTitleAd = document.querySelect('.photoboard__button');
+
+sortTitleAd.addEventListener('click', function () {
+	// alert(titleAd.sort());
+	alert('Hello');
+});
+// alert(titleAd.sort());
