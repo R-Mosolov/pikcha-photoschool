5ed7d1f395af838b66bb0114826d526e64bde507
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-04 14:23:52 +0300

    First Screen Is Reworked
    Added banner with 3 slides, site title and first 3 cards of photographers.

--- site/photoboard.pikcha-photoschool.ru/app/js/app.js
+++
@@ -1,8 +0,0 @@
-var titleAd = ['Владислав', 'Борис', 'Алексей'];
-var sortTitleAd = document.querySelect('.photoboard__button');
-
-sortTitleAd.addEventListener('click', function () {
-	// alert(titleAd.sort());
-	alert('Hello');
-});
-// alert(titleAd.sort());
