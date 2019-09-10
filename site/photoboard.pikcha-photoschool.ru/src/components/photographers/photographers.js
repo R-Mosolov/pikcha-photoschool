f513041efb0ec7abdc807fc2bae8a03eaf8c39fd
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-10 12:56:44 +0300

    Generator of Photographers Array
    Created 2 functions in Photographers component. First function create template of one photographer. And second function create array generator which apply layout template as argument.

--- site/photoboard.pikcha-photoschool.ru/src/components/photographers/photographers.js
+++ site/photoboard.pikcha-photoschool.ru/src/components/photographers/photographers.js
@@ -4,35 +4,32 @@ import OlegVaidner from './img/oleg-vaidner.jpg';
 import Database from './database';
 
 const Photographers = () => {
+
+    let createOnePhotographer = (name, price) => {
+        return(
+            <div className="card shadow" style={{width: "20rem"}}>
+                <img src={OlegVaidner} className="card-img-top" alt="Олег Вайднер"/>
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">Цена часа (руб.): {price}</p>
+                    <a href="#" className="btn btn-secondary">Портфолио</a>
+                    <a href="#" className="btn btn-success ml-3">Контакты</a>
+                </div>
+            </div>
+        )
+    };
+
+    let createPhotographersArray = () => {
+        let result = [];
+        for (let i = 0; i < Database.length; i++) {
+            result.push(createOnePhotographer(Database[i].name, Database[i].price));
+        }
+        return result;
+    };
+
     return(
         <div className="photographers d-flex justify-content-between mb-5">
-            <div className="card shadow" style={{width: '20rem'}}>
-                <img src={OlegVaidner} className="card-img-top" alt="Олег Вайднер"/>
-                <div className="card-body">
-                    <h5 className="card-title">{Database[0].name}</h5>
-                    <p className="card-text">Цена часа (руб.): {Database[0].price}</p>
-                    <a href="#" className="btn btn-secondary">Портфолио</a>
-                    <a href="#" className="btn btn-success ml-3">Контакты</a>
-                </div>
-            </div>
-            <div className="card shadow" style={{width: '20rem'}}>
-                <img src={OlegVaidner} className="card-img-top" alt="Олег Вайднер"/>
-                <div className="card-body">
-                    <h5 className="card-title">{Database[1].name}</h5>
-                    <p className="card-text">Цена часа (руб.): {Database[1].price}</p>
-                    <a href="#" className="btn btn-secondary">Портфолио</a>
-                    <a href="#" className="btn btn-success ml-3">Контакты</a>
-                </div>
-            </div>
-            <div className="card shadow" style={{width: '20rem'}}>
-                <img src={OlegVaidner} className="card-img-top" alt="Олег Вайднер"/>
-                <div className="card-body">
-                    <h5 className="card-title">{Database[2].name}</h5>
-                    <p className="card-text">Цена часа (руб.): {Database[2].price}</p>
-                    <a href="#" className="btn btn-secondary">Портфолио</a>
-                    <a href="#" className="btn btn-success ml-3">Контакты</a>
-                </div>
-            </div>
+            {createPhotographersArray()}
         </div>
     )
 };
